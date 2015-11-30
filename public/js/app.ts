7911b541a1d40afbff0e3db574526d3148be0558
Mauricio Farinelli <maufarinelli@gmail.com>
2015-11-30 16:43:13 -0500

    Tuto Angular Tour of Heroes

--- public/js/app.ts
+++ public/js/app.ts
@@ -1,7 +1,29 @@
-import {bootstrap, Component} from 'angular2/angular2';
+import {bootstrap, Component, NgModel} from 'angular2/angular2';
+
+class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <div><input [(ng-model)]="hero.name" placeholder="name"></div>
+        </div>
+        `,
+    directives: [NgModel]
 })
-class AppComponent { }
+class AppComponent {
+    public title: 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
 bootstrap(AppComponent);
