3bb956eac40be40e300eb1ad08ddc09ace8cf97e
Mauricio Farinelli <maufarinelli@gmail.com>
2015-12-08 16:56:28 -0500

    Raw product component, typescript compiler

--- public/js/app.ts
+++ public/js/app.ts
@@ -1,29 +1,61 @@
-import {bootstrap, Component, FORM_DIRECTIVES} from 'angular2/angular2';
+import {bootstrap, Component, FORM_DIRECTIVES, CORE_DIRECTIVES} from 'angular2/angular2';
 
-class Hero {
+// Products
+class Product {
     id: number;
-    name: string;
+    title: string;
+    img: string;
+    price: string;
 }
+
+var productsList: Product[] = [
+    {
+        id: 100,
+        title: 'Carrinho de Bebe',
+        img: 'http://placehold.it/140x100',
+        price: '199,90',
+    },
+    {
+        id: 101,
+        title: 'Mamadeira para Bebe',
+        img: 'http://placehold.it/140x100',
+        price: '29,90',
+    },
+    {
+        id: 102,
+        title: 'Fralda Pampers - Pacote',
+        img: 'http://placehold.it/140x100',
+        price: '49,90',
+    }
+];
 
 @Component({
-    selector: 'my-app',
+    selector: 'product',
     template: `
-        <h1>{{title}}</h1>
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-            <label>name: </label>
-            <div><input [(ng-model)]="hero.name" placeholder="name"></div>
-        </div>
+        <h1>Todos os produtos</h1>
+        <ul class="products-list">
+          <li *ng-for="#product of products"
+            (click)="onSelect(product)"
+            [ng-class]="getSelectedClass(product)">
+            <a class="product-wrapper">
+                <h3>{{product.title}}</h3>
+                <img src="{{product.img}}">
+                <p>{{product.price}}</p>
+            </a>
+          </li>
+        </ul>
         `,
+    styles: [`
+        .products-list {list-style-type: none}
+        .products-list li {display: inline-block; margin-right: 10px}
+        .products-list a {color: #000;}
+    `],
     directives: [FORM_DIRECTIVES]
 })
-class AppComponent {
-    public title: 'Tour of Heroes';
-    public hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+class ProductComponent {
+    public products = productsList;
+    onSelect() {};
+    getSelectedClass() {};
 }
 
-bootstrap(AppComponent);
+bootstrap(ProductComponent);
